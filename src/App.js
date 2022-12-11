47b6993239da41f1c88ad6e84983e92cae5252ea
Maya Lavi <maya.lavi11@gmail.com>
2022-12-11 01:00:50 -0500

    deploy to github pages

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ class App extends Component {
 
     // Important: Include the "basename" in Router, which is needed for deploying the React app to GitHub Pages
     return (
-      <Router basename="/bank-of-react-example-code-gh-pages">
+      <Router basename="/webdev-assignment-4-bank-of-react">
         <div>
           <Route exact path="/" render={HomeComponent}/>
           <Route exact path="/userProfile" render={UserProfileComponent}/>
